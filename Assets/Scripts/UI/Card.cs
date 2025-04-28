6e2868d22538b6b4248feca035364ce4ed8d9a61
rileyzzz <rileylemmler@gmail.com>
2025-04-27 19:40:29 -0500

    card hints

--- Assets/Scripts/UI/Card.cs
+++ Assets/Scripts/UI/Card.cs
@@ -9,12 +9,16 @@ public class Card : MonoBehaviour
     public TextMeshProUGUI m_MoneyImpactText;
     public TextMeshProUGUI m_PollutionImpactText;
     public TextMeshProUGUI m_HappinessImpactText;
+
+    public GameObject m_HintTextBox;
+    public TextMeshProUGUI m_HintText;
     private CardData m_cardData;
 
     public CardData CardData => m_cardData;
 
     private void Start()
     {
+        m_HintTextBox.SetActive(false);
     }
 
     private static void SetValueText(TextMeshProUGUI text, int value, string prefix, in Color goodColor, in Color badColor)
@@ -35,12 +39,16 @@ public class Card : MonoBehaviour
             text.text = "0";
         }
     }
+
     public void SetData(CardData data)
     {
+        m_HintTextBox.SetActive(false);
+
         m_cardData = data;
 
         m_NameText.text = data.Name;
         m_DescriptionText.text = data.Description;
+        m_HintText.text = data.HintText;
 
         SetValueText(m_MoneyImpactText, -data.Cost, "$", Color.green, Color.red);
         SetValueText(m_PollutionImpactText, data.EstimatedPollution, "", Color.gray, Color.green);
@@ -51,5 +59,10 @@ public class Card : MonoBehaviour
         Debug.Log($"set card to {data.Name}");
 
         // setup the text fields from card data
+    }
+
+    public void ToggleHint()
+    {
+        m_HintTextBox.SetActive(!m_HintTextBox.activeSelf);
     }
 }
